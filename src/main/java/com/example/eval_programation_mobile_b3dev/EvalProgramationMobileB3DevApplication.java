b539767a56e76342021f7e8fea556fd1cf3677b4
Tatrite <florian.fuchs31700@gmail.com>
2026-06-18 09:25:27 +0200

    feat: add project

---
+++ src/main/java/com/example/eval_programation_mobile_b3dev/EvalProgramationMobileB3DevApplication.java
@@ -0,0 +1,13 @@
+package com.example.eval_programation_mobile_b3dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalProgramationMobileB3DevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalProgramationMobileB3DevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eval_programation_mobile_b3dev/EvalProgramationMobileB3DevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eval_programation_mobile_b3dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalProgramationMobileB3DevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
